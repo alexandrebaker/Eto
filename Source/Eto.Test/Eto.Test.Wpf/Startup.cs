a2250b958b940645c4d8fbc32092602f6fb9376a
Alexandre Baker <alexandre.baker@gmail.com>
2014-09-23 12:09:24 -0400

    Test for CI trigger rules

--- Source/Eto.Test/Eto.Test.Wpf/Startup.cs
+++ Source/Eto.Test/Eto.Test.Wpf/Startup.cs
@@ -3,20 +3,19 @@ using System;
 
 namespace Eto.Test.Wpf
 {
-	class Startup
-	{
-		[STAThread]
-		static void Main(string[] args)
-		{
-			var generator = new Eto.Wpf.Platform();
+    class Startup
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var generator = new Eto.Wpf.Platform();
 
-			// don't use tiling for the direct drawing test
-			Style.Add<DrawableHandler>("direct", handler => handler.AllowTiling = false);
+            // don't use tiling for the direct drawing test
+            Style.Add<DrawableHandler>("direct", handler => handler.AllowTiling = false);
 
-			var app = new TestApplication(generator);
-			app.Run();
-		}
-
-	}
+            var app = new TestApplication(generator);
+            app.Run();
+        }
+    }
 }
 
